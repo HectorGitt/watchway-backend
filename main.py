5f558c2f0cb5ba349f5c4f2eb623707cadfcff08
Deniyi <adeniyi.olaitanhector@yahoo.com>
2026-02-17 18:25:48 +0100

    fixed the backend crash.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status, BackgroundTasks
+from fastapi import FastAPI, Depends, HTTPException, status, BackgroundTasks, Header
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
